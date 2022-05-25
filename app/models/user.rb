690477accbe77411bca1d1c7a1a90b8d1d8677ad
jin <0cu2045d@gmail.com>
2022-05-26 00:00:10 +0900

    Userバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-    has_secure_password
+has_secure_password
+
+validates :name, presence: true
+validates :email, presence: true, uniqueness: { case_sensitive: false }
+validates :password, presence: true, length: { minimum: 6 }
 end
