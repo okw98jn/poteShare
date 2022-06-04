eff203c4fca77694df551f575c98020f63e1c0b6
jin <0cu2045d@gmail.com>
2022-06-04 18:01:32 +0900

    carrierwave導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
 has_secure_password
+mount_uploader :image_name, ImageNameUploader
 
 validates :name, presence: true
 validates :email, presence: true, uniqueness: { case_sensitive: false }
-validates :password, presence: true, length: { minimum: 6 }
+validates :password, presence: true, length: { minimum: 6 },on: :create
+validates :password_confirmation,presence: true,on: :create
+
 end
