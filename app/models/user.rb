ded025772a62250346bc15bf6f45d92b38fc0c1d
jin <0cu2045d@gmail.com>
2022-06-07 21:49:16 +0900

    annotate導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string(255)
+#  image_name      :string(255)
+#  introduction    :text(65535)
+#  name            :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
 has_secure_password
 mount_uploader :image_name, ImageNameUploader
 
 validates :name, presence: true
+validates :image_name, presence: true
 validates :email, presence: true, uniqueness: { case_sensitive: false }
+validates :introduction, presence: true, length: { maximum: 140 }
 validates :password, presence: true, length: { minimum: 6 },on: :create
 validates :password_confirmation,presence: true,on: :create
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string(255)
+#  image_name      :string(255)
+#  introduction    :text(65535)
+#  name            :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
