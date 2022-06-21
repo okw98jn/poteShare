625707fe8b09c55407e5580b58d81d2d2471f3f6
jin <0cu2045d@gmail.com>
2022-06-22 00:32:51 +0900

    予約フォームバリデーション

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,8 +4,9 @@
 #
 #  id         :bigint           not null, primary key
 #  end_date   :datetime         not null
-#  number     :integer          not null
+#  number     :integer
 #  start_date :datetime         not null
+#  sum_price  :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  room_id    :bigint           not null
@@ -24,4 +25,41 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :number, presence: true
+  validate :date_before_start
+  validate :date_before_end
+  validate :same_date
+  require "date"
+
+  #開始日のバリデーション
+  def date_before_start
+    if start_date == nil
+      return
+    elsif start_date < Date.today
+      errors.add(:start_date, "は今日以降のものを選択してください")
+    end
+  end
+
+  #終了日のバリデーション
+  def date_before_end
+    if end_date == nil
+      return
+    elsif end_date < Date.today
+      errors.add(:end_date, "は今日以降のものを選択してください")
+    elsif start_date > end_date
+      errors.add(:end_date, "は開始日以降のものを選択してください")
+    end
+  end
+  
+  #開始日と終了日が同じ日のバリデーション
+  def same_date
+    if start_date == nil || end_date == nil
+      return
+    elsif start_date == end_date
+      errors.add(:start_date,"と終了日を同じ日にしないで下さい")
+    end
+  end
 end
